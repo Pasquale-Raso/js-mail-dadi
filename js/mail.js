e361cf806f2fa326f8a96f138921422231a21f37
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-07-02 14:48:12 +0200

    modifiche-js-mail

--- js/mail.js
+++ js/mail.js
@@ -6,15 +6,10 @@ stampa un messaggio appropriato sull’esito del controllo.
 
 */
 
-// 1 - 
-// 2 - 
-// 3 - 
-// 4 - 
-// 5 - 
-// 6 - 
-// 7 - 
-// 8 - 
-// 9 - 
+// 1 - Chiedi all’utente la sua email
+// 2 - creare una lista di email
+// 3 - ricerca email scritta nella lista
+// 4 - stampa se c'è o non c'è
 
 
 
