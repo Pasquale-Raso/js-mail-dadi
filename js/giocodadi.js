8370121fb26c8b371300bfca49b2e32cf3b54bba
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-07-02 14:43:34 +0200

    base-2-esercizi

---
+++ js/giocodadi.js
@@ -0,0 +1,19 @@
+/*
+
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto e stampare il risultato.
+
+*/
+
+// 1 - 
+// 2 - 
+// 3 - 
+// 4 - 
+// 5 - 
+// 6 - 
+// 7 - 
+// 8 - 
+// 9 - 
+
+
+
---
+++ js/mail.js
@@ -0,0 +1,20 @@
+/*
+
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+
+*/
+
+// 1 - 
+// 2 - 
+// 3 - 
+// 4 - 
+// 5 - 
+// 6 - 
+// 7 - 
+// 8 - 
+// 9 - 
+
+
+
